744773ac8faff4a06624f26d690c967277541aa4
cec247 <cec247@cornell.edu>
2013-02-13 20:00:16 +0000

    user model

--- db/migrate/20130206194213_create_micro_posts.rb
+++ db/migrate/20130206194213_create_micro_posts.rb
@@ -1,10 +1,8 @@
 class CreateMicroPosts < ActiveRecord::Migration
   def change
-	drop_table :micro_posts
     create_table :micro_posts do |t|
       t.integer :user_id
       t.string :content
-
       t.timestamps
     end
   end
